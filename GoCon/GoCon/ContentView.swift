f07040da9dc282d2f06362d14df0c21742986253
james-mcdougall <jameswilliammcdougall@gmail.com>
2023-11-09 09:55:11 -0800

    Initial project commit

---
+++ GoCon/GoCon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GoCon
+//
+//  Created by James McDougall on 11/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GoCon/GoCon/GoConApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoConApp.swift
+//  GoCon
+//
+//  Created by James McDougall on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct GoConApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
